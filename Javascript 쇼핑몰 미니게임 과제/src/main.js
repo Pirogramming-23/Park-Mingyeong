83f61e21d93d817434a39c30d73077fdee0391c3
mingyeongpark12 <shangh21@cau.ac.kr>
2025-07-02 23:34:06 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -3,7 +3,7 @@ function loadItems() {
     return fetch('data/data.json')
     .then(response => response.json())
     .then(json => json.items);
-        }
+}
 
 // Update the list
 function displayItems(items) {
@@ -19,10 +19,32 @@ function createHTMLString(item) {
     </li>
     `;
 }
+
+function onButtonClick(event, items) {
+    const target = event.target;
+    const key = target.dataset.key;
+    const value = target.dataset.value;
+    
+    if (key == null || value == null) {
+        return;
+    }
+    
+    const filtered = items.filter(item => item[key] === value);
+    console.log(filtered);
+    displayItems(filtered);
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems()
     .then(items => {
         displayItems(items);
-    //  setEventListeners(items)
-})
-.catch(console.log);
+        setEventListeners(items);
+    })
+    .catch(console.log);
